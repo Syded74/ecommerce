0ea972d8662a042aeaac03cee40d6d22e66f666b
Syded74 <47427269+Syded74@users.noreply.github.com>
2024-06-19 21:57:40 +0000

    Update image paths for products

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,7 +15,7 @@
         @foreach($products as $product)
             <div class="col-md-4">
                 <div class="card">
-                    <img class="card-img-top" src="{{ asset('storage/images/' . $product->image) }}" alt="{{ $product->name }}">
+                    <img class="card-img-top" src="{{ asset('storage/products/' . $product->image) }}" alt="{{ $product->name }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $product->name }}</h5>
                         <p class="card-text">{{ $product->description }}</p>
